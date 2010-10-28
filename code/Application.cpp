05c9fff9a0ec82583793903bd1abe7db83f66fa4
Christopher J. Covert <covertcj@rose-hulman.edu>
2010-10-28 03:09:56 +0000

    Added some basecode.

---
+++ code/Application.cpp
@@ -0,0 +1,17 @@
+/*
+ * Application.cpp
+ *
+ *  Created on: Oct 27, 2010
+ *      Author: covertcj
+ */
+
+#include "Application.h"
+
+Application::Application() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Application::~Application() {
+	// TODO Auto-generated destructor stub
+}
---
+++ code/Application.h
@@ -0,0 +1,17 @@
+/*
+ * Application.h
+ *
+ *  Created on: Oct 27, 2010
+ *      Author: covertcj
+ */
+
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+class Application {
+public:
+	Application();
+	virtual ~Application();
+};
+
+#endif /* APPLICATION_H_ */
