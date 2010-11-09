6f24915b09b38c05d526c9dcf158583c19085db5
Christopher J. Covert <covertcj@rose-hulman.edu>
2010-11-09 00:44:57 +0000

    Added an enemy stub.

---
+++ code/Enemy.cpp
@@ -0,0 +1,17 @@
+/*
+ * Enemy.cpp
+ *
+ *  Created on: Nov 8, 2010
+ *      Author: covertcj
+ */
+
+#include "/home/covertcj/Documents/courses/junior/fall/cg/final-project/trunk/code/Enemy.h"
+
+Enemy::Enemy() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Enemy::~Enemy() {
+	// TODO Auto-generated destructor stub
+}
---
+++ code/Enemy.h
@@ -0,0 +1,17 @@
+/*
+ * Enemy.h
+ *
+ *  Created on: Nov 8, 2010
+ *      Author: covertcj
+ */
+
+#ifndef ENEMY_H_
+#define ENEMY_H_
+
+class Enemy {
+public:
+	Enemy();
+	virtual ~Enemy();
+};
+
+#endif /* ENEMY_H_ */
